80b6a0bf997dd4a76ce93a007dd9f0c3b53d568c
Debasmita Pal <mm858923945@gmail.com>
2024-05-16 21:07:48 +0530

    Initial commit

---
+++ routes/AuthUrl.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const customMiddleware = require('../middleware/customMidddleware');
+const authenticationController = require("../controller/auth/authentication")
+const userController = require('../controller/auth/userApi');
+
+const authenticationMiddleware = require('../middleware/authenticate');
+
+router.post('/register', userController.registerUser);
+router.get('/forGotPasswordOnUserId', customMiddleware, userController.forgotPasswordOnUserId);
+router.post('/login', userController.loginUser);
+router.post('/logout', authenticationMiddleware.checkSessionMiddleware, (req, res) => {
+  // Assuming the cookie name is 'session'
+  res.clearCookie('refresh_token', {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true
+  });
+
+  // Send a response indicating successful logout
+  res.status(200).send('Logged out successfully');
+});
+router.get("/session", authenticationMiddleware.checkSessionMiddleware, authenticationController.grantPermission);
+router.get('/refresh', authenticationMiddleware.checkSessionMiddleware, authenticationMiddleware.checkAccessTokenMiddleWare, authenticationController.newAccessToken);
+
+router.post('/passWordResetVerification', customMiddleware, userController.passWordResetVerification);
+router.post('/emailVerifyUser', customMiddleware, userController.emailVerifyUser);
+router.post('/updateUserEmail', customMiddleware, userController.updateUserEmail);
+router.post('/updateUserBasicData', customMiddleware, userController.updateUserBasicData);
+router.post('/roleAccess/AssignRoleToUser', customMiddleware, userController.AssignRoleToUser);
+
+router.get('/getUsers',authenticationMiddleware.checkSessionMiddleware,authenticationController.getuser)
+module.exports = router;
