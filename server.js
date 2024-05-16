039e378f32864a024150afa4a5817617795eb215
Debasmita Pal <mm858923945@gmail.com>
2024-05-16 21:32:28 +0530

    done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');// Read Application Cookies
 const path = require('path');
 //scheduler lock file should be removed before running the scheduler
 const corsOption = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://personal-vse6.onrender.com"],
   credentials: true
 };
 const AuthRoutes = require('./routes/AuthUrl');
